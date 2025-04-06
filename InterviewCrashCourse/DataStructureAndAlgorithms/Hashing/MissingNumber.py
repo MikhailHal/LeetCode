73279f5e4cf1976ebfd551bfafc2b6dcd2a77b16
Haruto K <aoi.stella.dev@gmail.com>
2025-04-06 21:05:52 +0900

    solve MissingNumber more efficient

--- InterviewCrashCourse/DataStructureAndAlgorithms/Hashing/MissingNumber.py
+++ InterviewCrashCourse/DataStructureAndAlgorithms/Hashing/MissingNumber.py
@@ -1,8 +1,14 @@
 from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        """
         numsSet = set(nums)
         n = len(nums)
         for i in range(n+1):
             if not i in numsSet:
                 return i
+        """
+        n = len(nums)
+        expectedSum = n * (n+1) // 2
+        actualSum = sum(nums)
+        return expectedSum - actualSum
