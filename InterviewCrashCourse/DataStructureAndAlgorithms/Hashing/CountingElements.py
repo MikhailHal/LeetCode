996112d7c37308c07405b234c5a8639539d0ef9a
Haruto K <aoi.stella.dev@gmail.com>
2025-04-06 21:26:36 +0900

    solve CountingElements more efficient

--- InterviewCrashCourse/DataStructureAndAlgorithms/Hashing/CountingElements.py
+++ InterviewCrashCourse/DataStructureAndAlgorithms/Hashing/CountingElements.py
@@ -1,15 +1,11 @@
 from typing import List
 class Solution:
     def countElements(self, arr: List[int]) -> int:
-        plusOneDict = {}
-        ans = 0
-        for i in arr:
-            if not i in plusOneDict:
-                plusOneDict[i] = i+1
-        
-        for i in arr:
-            if plusOneDict[i] in arr:
-                ans += 1
-        return ans
+        element_set = set(arr)
+        count = 0
+        for num in arr:
+            if num + 1 in element_set:
+                count += 1         
+        return count
     
-Solution().countElements([1,1,3,3,5,5,7,7])
+Solution().countElements([1,1,2])
