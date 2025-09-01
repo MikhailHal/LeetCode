47fd5277cfd5699cbc50080f450c8f7c51f5b928
Haruto K <aoi.stella.dev@gmail.com>
2025-09-01 19:07:45 +0900

    add: resolve 917

--- 917.reverse-only-letters.py
+++ 917.reverse-only-letters.py
@@ -7,6 +7,19 @@
 # @lc code=start
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        
+        left = 0
+        right = len(s) - 1
+        ans = list(s)
+
+        while left < right:
+            while left < right and not s[left].isalpha():
+                left += 1
+            while right > left and not s[right].isalpha():
+                right -= 1
+            ans[left], ans[right] = ans[right], ans[left]
+            left += 1
+            right -= 1
+        return ''.join(ans)
 # @lc code=end
 
+print(Solution().reverseOnlyLetters("ab-cd"))
