003b1e68556b028c03289eb3e8c13f6b9975f617
Haruto K <aoi.stella.dev@gmail.com>
2025-09-03 00:49:41 +0900

    resolve 2460

--- 2460.apply-operations-to-an-array.py
+++ 2460.apply-operations-to-an-array.py
@@ -5,8 +5,20 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        
+        left = 0
+        for right in range(len(nums) - 1):
+            if nums[right] == nums[right + 1]:
+                nums[right] *= 2
+                nums[right + 1] = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        return nums
 # @lc code=end
 
+print(Solution().applyOperations([1,2,2,1,1,0]))
