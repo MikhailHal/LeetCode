5b294df14bb803ca611bed43e0cead9660aeb2a3
Haruto K <aoi.stella.dev@gmail.com>
2025-09-03 00:33:14 +0900

    add 2460

---
+++ 2460.apply-operations-to-an-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2460 lang=python3
+#
+# [2460] Apply Operations to an Array
+#
+
+# @lc code=start
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        
+# @lc code=end
+
