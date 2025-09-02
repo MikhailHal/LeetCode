54fbc2a6301082ea0b9da39c68ce89a314cc9221
Haruto K <aoi.stella.dev@gmail.com>
2025-09-02 23:07:58 +0900

    add 2540

---
+++ 2540.minimum-common-value.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2540 lang=python3
+#
+# [2540] Minimum Common Value
+#
+
+# @lc code=start
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        
+# @lc code=end
+
