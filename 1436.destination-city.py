d70689c9a719329cd93fb342e7155cacec7c44bd
Haruto K <aoi.stella.dev@gmail.com>
2025-09-05 21:54:54 +0900

    resolve 1436 more efficient

--- 1436.destination-city.py
+++ 1436.destination-city.py
@@ -8,19 +8,12 @@
 from typing import List
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        cityMap = {}
-        # 各pathの0番目の要素をキー・1番目の要素をバリューとして格納
-        # 各pathの1番目の要素をキーとして格納。バリューは空文字
+        dep = set()
+        des = set()
         for path in paths:
-            depature = path[0]
-            arrive = path[1]
-            cityMap[depature] = arrive
-            if not arrive in cityMap:
-                cityMap[arrive] = ""
-        for key in cityMap:
-            if cityMap[key] == "":
-                return key
-        # バリューの中身がからのものが答え
+            dep.add(path[0])
+            des.add(path[1])
+        return (des - dep).pop()
 # @lc code=end
 
 print(Solution().destCity([["London","New York"]]))
