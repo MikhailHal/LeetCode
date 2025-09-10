439835ee59474f868602038fa0af21c7f13c1871
Haruto K <aoi.stella.dev@gmail.com>
2025-09-10 23:47:54 +0900

    resolve 560

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -6,11 +6,24 @@
 
 # @lc code=start
 from typing import List
-from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # 復習のためコードは消しました。前の回答みたければGitHub見て
-        pass
+        dict = {0: 1}
+        ans = 0
+        sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            
+            target_key = sum - k
+            if target_key in dict:
+                ans += dict[target_key]
+            if sum in dict:
+                dict[sum] += 1
+            else:
+                dict[sum] = 1
+        return ans
 # @lc code=end
 
-Solution().subarraySum(nums = [1], k = 0)
+print(Solution().subarraySum(nums = [1,2,3], k = 3)) #2
+print(Solution().subarraySum(nums = [1,1,1], k = 2)) #2
+print(Solution().subarraySum(nums = [1], k = 0)) #0
