12ee5bccdc21203385faeb87c82d4e8389486973
Haruto K <aoi.stella.dev@gmail.com>
2025-09-10 23:52:28 +0900

    reset 560

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -8,20 +8,7 @@
 from typing import List
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dict = {0: 1}
-        ans = 0
-        sum = 0
-        for i in range(len(nums)):
-            sum += nums[i]
-            
-            target_key = sum - k
-            if target_key in dict:
-                ans += dict[target_key]
-            if sum in dict:
-                dict[sum] += 1
-            else:
-                dict[sum] = 1
-        return ans
+        return
 # @lc code=end
 
 print(Solution().subarraySum(nums = [1,2,3], k = 3)) #2
