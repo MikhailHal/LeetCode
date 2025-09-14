c482508313a9cd0baa6f9742a5ed2a247d9c7abe
Haruto K <aoi.stella.dev@gmail.com>
2025-09-15 00:23:35 +0900

    resolve 283

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -11,10 +11,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # 復習のためコードは消しました。前の回答みたければGitHub見て
-        pass
+        read = write = 0
+        for read in range(len(nums)):
+            if nums[read] != 0:
+                nums[write] = nums[read]
+                write += 1
+        for i in range(write, len(nums)):
+            nums[i] = 0
         print(nums)
         
 # @lc code=end
 
 Solution().moveZeroes([0,1,2])
+Solution().moveZeroes([1,0,1])
+Solution().moveZeroes([1])
+Solution().moveZeroes([0,1])
+Solution().moveZeroes([0,1,0,3,12])
