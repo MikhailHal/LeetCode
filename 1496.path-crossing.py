881a8b82b83bff3d2b9eb52582759c181700123a
Haruto K <aoi.stella.dev@gmail.com>
2025-09-15 16:12:56 +0900

    resolve 1496

--- 1496.path-crossing.py
+++ 1496.path-crossing.py
@@ -7,10 +7,25 @@
 # @lc code=start
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        # 復習のためコードは消しました。前の回答みたければGitHub見て
-        pass
+        pos_visit = set()
+        pos_visit.add((0, 0))
+        x = y = 0
+        for unit in path:
+            if unit == 'N':
+                y += 1
+            elif unit == 'S':
+                y -= 1
+            elif unit == 'E':
+                x += 1
+            else:
+                x -= 1
+            if (x, y) in pos_visit:
+                return True
+            pos_visit.add((x, y))
+        return False
 
         
 # @lc code=end
-
-print(Solution().isPathCrossing("NESW"))
+print(Solution().isPathCrossing("NES")) # false
+print(Solution().isPathCrossing("NESW")) # true
+print(Solution().isPathCrossing("N")) # false
