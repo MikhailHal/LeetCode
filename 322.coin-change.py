fb72b682c1cee7d6d5b5ec948718eb4216e08705
Haruto K <aoi.stella.dev@gmail.com>
2025-09-15 17:16:52 +0900

    resolve 322

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -10,13 +10,12 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
-        for i in range(1,amount+1):
+        for i in range(amount + 1):
             for coin in coins:
-                if i < coin:
-                    continue
-                dp[i] = min(dp[i], dp[i-coin]+1)
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
         if dp[amount] == float('inf'):
-            dp[amount] = -1
+            return -1
         return dp[amount]
         
 # @lc code=end
