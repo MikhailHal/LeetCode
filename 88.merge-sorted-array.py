91d33673b839057bb657c4bcd196b0a6dcccdf3d
Haruto K <aoi.stella.dev@gmail.com>
2025-09-20 00:00:10 +0900

    resolve 88

---
+++ 88.merge-sorted-array.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        sorted_merged_array = sorted(nums1[:m] + nums2)
+        for i, elem in enumerate(sorted_merged_array):
+            nums1[i] = elem
+        return None
+        
+# @lc code=end
+
+Solution().merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
+# [1,2,2,3,5,6]
+
+Solution().merge(nums1 = [1], m = 1, nums2 = [], n = 0)
+# [1]
+
+Solution().merge(nums1 = [0], m = 0, nums2 = [1], n = 1)
+# [1]
