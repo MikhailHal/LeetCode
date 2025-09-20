1e7cd8494dd544caed9df8bd92a63f5990ebb764
Haruto K <aoi.stella.dev@gmail.com>
2025-09-20 10:57:45 +0900

    resolve 121

---
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = float('inf')
+        ans = 0
+        for price in prices:
+            buy = min(buy, price)
+            profit = price - buy
+            ans = max(ans, profit)
+        print(ans)
+        return ans
+# @lc code=end
+
+Solution().maxProfit(prices = [2,4,1])
+# 2
+
+Solution().maxProfit(prices = [7,1,5,3,6,4])
+# 5
+
+Solution().maxProfit(prices = [7,6,4,3,1])
+# 0
+
+Solution().maxProfit(prices = [1])
+# 0
