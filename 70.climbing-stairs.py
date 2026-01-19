8516b190efd7db5a2a7594681eef5575256b51a1
Haruto K <aoi.stella.dev@gmail.com>
2026-01-19 13:55:49 +0900

    resolve 70

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -5,17 +5,31 @@
 #
 
 # @lc code=start
+"""
+部分問題
+i段目時点で、その段数にたどり着く方法が何通りあるか。
+
+例外
+i=0,1,2は手動初期化
+
+状態
+i段目へ上れる通りの数
+
+状態遷移
+dp[i] = dp[i-1] + dp[i-2]
+
+"""
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
         dp = [0] * (n+1)
-        dp[0] = 1
         dp[1] = 1
-        for i in range(2, n+1):
+        dp[2] = 2
+        for i in range(3,n+1):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
 # @lc code=end
-
-print(Solution().climbStairs(1)) # 1
-print(Solution().climbStairs(2)) # 2
-print(Solution().climbStairs(3)) # 3
-print(Solution().climbStairs(4)) # 2
+print(Solution().climbStairs(1)) #1
+print(Solution().climbStairs(2)) #2
+print(Solution().climbStairs(3)) #3
