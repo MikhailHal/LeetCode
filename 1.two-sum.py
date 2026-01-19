7d9c5eae78c2e2ee97a65a1de731c5bbf27b75c7
Haruto K <aoi.stella.dev@gmail.com>
2026-01-19 12:55:28 +0900

    resolve 1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -4,15 +4,34 @@
 # [1] Two Sum
 #
 
+"""
+部分問題
+・nums[i]に対する、targetを構成するための補数(complement)が存在するかどうか
+
+状態
+・補数インデックス辞書(キー：nums[i] / バリュー：i)
+
+ 漸化式
+    if complement in seen:
+        return [i, seen[complement]]
+    seen[num] = i
+
+例外
+・numsのサイズが2の場合
+
+"""
 # @lc code=start
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in dict:
-                return [dict[complement], i]
-            dict[nums[i]] = i
-        return [0, 0]
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [i, seen[complement]]
+            seen[num] = i
 # @lc code=end
+
+print(Solution().twoSum(nums = [2,7,11,15], target = 9))
+print(Solution().twoSum(nums = [3,2,4], target = 6))
+print(Solution().twoSum(nums = [3,3], target = 6))
