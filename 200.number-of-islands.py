8914ca37752496a8785b106519fd802fca94bf1d
Haruto K <aoi.stella.dev@gmail.com>
2026-02-01 21:49:37 +0900

    resolve 200

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -6,62 +6,65 @@
 
 # @lc code=start
 """
-・部分問題(本流)
-    ・現ノードが陸かつ未訪問の場合カウント
-
+・部分問題(main)
+    目的：(0,0)~(r_max,c_max)まで走査し時点での島数を数える
+    引数：-
+    ・(0,0)から最後まで探索開始
+    ・現座標が1かつ未訪問であればdfs開始
+    ・ansカウント
 ・部分問題(dfs)
-    ・現ノードが範囲外であれば終了
-    ・現ノードが陸であれば訪問済みとしてマーク
-    ・前後左右に対し走査開始
+    目的：(r,c)自身から派生する陸を走査
+    引数：座標(r,c)
+    ・現座標が範囲外だったらスキップ
+    ・現座標が0だったらスキップ
+    ・現座標が訪問済みだったらスキップ
+    ・現座標を訪問済みにする
+    ・上下左右に同dfs処理を呼ぶ
 
 ・例外
-    ・上記部分問題を破壊するケースはなし
+    部分問題を破壊する例外はない
 
 ・状態
-    ・島の数(int)
-    ・訪問状態(List[List[bool]])
+    ・訪問状態：visited[List[bool]]
+    ・島数：ans int
 
-・状態遷移
-    ・上方向dfs：dfs(row-1, column)
-    ・下方向dfs：dfs(row+1, column)
-    ・左方向dfs：dfs(row, column-1)
-    ・右方向dfs：dfs(row, column+1)
+・遷移
+    if 範囲外:
+        return
+    if grid[r][c]が水(==0):
+        return
+    if visited[r][c]が訪問済み:
+        return
+    __dfs(r - 1, c)
+    __dfs(r + 1, c)
+    __dfs(r, c - 1)
+    __dfs(r, c + 1)
+
 """
 from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        max_row = len(grid)
-        max_column = len(grid[0])
-
+        max_row, max_column = len(grid), len(grid[0])
         ans = 0
-        seen: List[List[bool]] = [[False] * max_column for _ in range(max_row)]
-        def __markIsland(row: int, column: int):
-            if row < 0 or row >= max_row:
+        visited = [[False] * max_column for _ in range(max_row)]
+        def __dfs(r: int, c: int):
+            nonlocal max_row, max_column, visited
+            if r < 0 or r >= max_row or c < 0 or c >= max_column:
                 return
-            if column < 0 or column >= max_column:
+            if grid[r][c] == "0" or visited[r][c] == True:
                 return
-            if grid[row][column] != "1":
-                return
-            if seen[row][column]:
-                return
-
-            seen[row][column] = True
-            __markIsland(row - 1, column)
-            __markIsland(row + 1, column)
-            __markIsland(row, column - 1)
-            __markIsland(row, column + 1)
+            
+            visited[r][c] = True
+            __dfs(r - 1, c)
+            __dfs(r + 1, c)
+            __dfs(r, c - 1)
+            __dfs(r, c + 1)
             return
-        
-        for row in range(max_row):
-            for column in range(max_column):
-                is_seen = seen[row][column]
-                if grid[row][column] == "1" and not is_seen:
+        for r in range(max_row):
+            for c in range(max_column):
+                if grid[r][c] == "1" and visited[r][c] == False:
+                    __dfs(r,c)
                     ans += 1
-                    seen[row][column] = True
-                    __markIsland(row - 1, column)
-                    __markIsland(row + 1, column)
-                    __markIsland(row, column - 1)
-                    __markIsland(row, column + 1)
         return ans
         
 # @lc code=end
